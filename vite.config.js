bc114768df6974bdf291eb23a0003a1c50ac1df0
harold <harold.s.f@hotmail.com>
2025-10-20 20:50:17 -0500

    feat: termino configuración del proyecto para despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName ='mini-ecommerce-deploy';
+
 // https://vite.dev/config/
 export default defineConfig({
+  // Configura la ruta base para el despliegue
+  base: `/${repoName}/`,
   plugins: [react()],
 })
